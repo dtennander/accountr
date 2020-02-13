cae9af44cbda528d2b8fdff469932c31c6c7715b
David Tennander <david.tennander@gmail.com>
2020-02-14 00:11:59 +0100

    Follow PORT env.

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,8 @@ module Main where
 import Servant
 import Network.Wai.Handler.Warp (run)
 import Api
+import System.Environment (lookupEnv)
+import Data.Maybe (fromMaybe)
 
 server :: Server MainAPI
 server = getJournal
@@ -17,4 +19,6 @@ app :: Application
 app = serve mainApi server
 
 main :: IO ()
-main = run 8081 app
+main = do
+  p <- fromMaybe "8080" <$> lookupEnv "PORT"
+  run (read p) app
